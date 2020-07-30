03a96cb072d69bf1939594b8433de01cb0bf7074
othaderek <otha.hernandez@gmail.com>
2020-07-30 19:45:37 -0400

    revised comments.

--- arrayPairSum.py
+++ arrayPairSum.py
@@ -11,14 +11,16 @@ def arrayPairSum(arr,k):
 
     # Loop through array and add nums to seen set
     for num in arr:
-        
+
         target = k-num
 
+        # If target isn't in the seen set add it
         if target not in seen:
             seen.add(num)
+        # If target is in seen set, add it and current num to ouput as tuple
         else:
             output.add((min(num,target), max(num,target)))
-        
+    # return length of output set
     return len(output)
 
 
