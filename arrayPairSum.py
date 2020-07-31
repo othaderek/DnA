23eb9674504082fcc6e6d1ff61a4dd88a0d8984a
othaderek <otha.hernandez@gmail.com>
2020-07-30 20:01:25 -0400

    Added comments

--- arrayPairSum.py
+++ arrayPairSum.py
@@ -1,27 +1,28 @@
 # Return the number of pairs in a given array who's sum is k
+# Array must be sorted in order to work
 
 def arrayPairSum(arr,k):
     # Base case
     if len(arr)<2:
         return
-    
     # Sets for tracking
     seen = set()
     output = set()
 
     # Loop through array and add nums to seen set
     for num in arr:
+        # reate target num by subtracting k from num
+        target = num-k
 
-        target = k-num
-
-        # If target isn't in the seen set add it
+        # If target isn't in the seen set add num
         if target not in seen:
             seen.add(num)
         # If target is in seen set, add it and current num to ouput as tuple
         else:
-            output.add((min(num,target), max(num,target)))
+            output.add((min(target, num), max(target, num)))
     # return length of output set
+    print(output)
     return len(output)
 
 
-print(arrayPairSum([1,2,3,4,5,], 4))
+print(arrayPairSum([-1,1,2,3,4,5], 4))
